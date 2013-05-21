7ddd7d79e8e9612816f075b6704c4b1b27ec58c9
Bookis <vegan.bookis@gmail.com>
2013-05-21 08:55:50 -0700

    add search

--- app.rb
+++ app.rb
@@ -3,13 +3,22 @@ require 'nokogiri'
 require "open-uri"
 
 class HitchWikiApp < Sinatra::Base
+  
+  get "/guide-search" do
+    redirect to("/#{params[:q].gsub(' ', '_')}")
+  end
+  
   get '/*' do
     url = "http://en.wikipedia.org/wiki/"
     url += params[:splat][0]
-    doc = Nokogiri::HTML(open(url)).at_css("#content")
+    begin
+      doc = Nokogiri::HTML(open(url)).at_css("#content")
+    rescue OpenURI::HTTPError, "404 Not Found"
+      halt 404
+    end
+      
     halt 404 unless doc
     doc.css("a").each do |link|
-      puts link.text.inspect
       link.attributes["href"].value = link.attributes["href"].value.gsub("wiki/", "") if link.attributes["href"]
     end
     
